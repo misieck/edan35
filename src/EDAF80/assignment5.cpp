595450ce6256931b052ba4e30c8f0abab2eb921d
Resende <pedrofilipe.pintoresende@gmail.com>
2023-11-02 14:43:41 +0000

    ray function and colision

--- src/EDAF80/assignment5.cpp
+++ src/EDAF80/assignment5.cpp
@@ -169,4 +169,77 @@ int main()
 	} catch (std::runtime_error const& e) {
 		LogError(e.what());
 	}
+
+
+	struct Point {
+    float x, y, z;
+};
+
+struct Vector {
+    float x, y, z;
+};
+
+struct Sphere {
+    Point c; // Center
+    float r; // Radius
+};
+
+// Dot product of two vectors
+float Dot(Vector a, Vector b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+// Test if ray intersects sphere
+int TestRaySphere(Point p, Vector d, Sphere s);
+
+int main() {
+    // Example usage
+    Point rayOrigin = {0.0f, 0.0f, 0.0f};
+    Vector rayDirection = {1.0f, 1.0f, 0.0f}; // Ray pointing along the x-axis
+    Sphere sphere = {{-1.0f, -1.0f, 0.0f}, 0.4f}; // Sphere at (1, 0, 0) with radius 1.0
+
+    int result = TestRaySphere(rayOrigin, rayDirection, sphere);
+
+    if (result)
+        std::cout << "Ray intersects the sphere." << std::endl;
+    else
+        std::cout << "Ray does not intersect the sphere." << std::endl;
+
+    return 0;
+}
+
+// Test if ray r = p + td intersects sphere s
+int TestRaySphere(Point p, Vector d, Sphere s) {
+    Vector m = {p.x - s.c.x, p.y - s.c.y, p.z - s.c.z};
+    float c = Dot(m, m) - s.r * s.r;
+
+    if (c <= 0.0f)
+        return 1;
+
+    float b = Dot(m, d);
+
+    if (b > 0.0f)
+        return 0;
+
+    float disc = b * b - c;
+
+    if (disc < 0.0f)
+        return 0;
+
+    return 1;
+}
+
+bool TestSphereSphere(Sphere a, Sphere b) {
+    // Calculate squared distance between centers
+    Vector d = a.position - b.position;
+    float squaredDistance = Dot(d, d); // Same as d.LengthSquared()
+
+    // Spheres intersect if squared distance is less than squared sum of radii
+    float radiusSum = a.radius + b.radius;
+    float squaredRadii = radiusSum * radiusSum;
+
+    return squaredDistance <= squaredRadii;
+}
+
+
 }
