72b07389a9908b4e0f49261417f48e76ac38ffdc
Resende <pedrofilipe.pintoresende@gmail.com>
2023-11-02 17:35:21 +0000

    velocity, position and size functions on collisions.cpp

--- src/EDAF80/collisions.cpp
+++ src/EDAF80/collisions.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
+#include <glm/gtx/io.hpp>
+#include <random>  
+#include <glm/glm.hpp>
+#include <cmath> 
+
+#define max_theta 90 
+#define min_theta 0
+#define max_phi 90 
+#define min_phi 0
+#define max_ro 100 
+#define min_ro 30
+#define max_z 50
+#define min_ast_size 1
+#define max_ast_size 4
+#define close_radius 10
+#define min_velocity_norm 1
+#define max_velocity_norm 3
+
+
+
+glm::vec3 generate_asteroid_position();
+float generate_asteroid_radius();
+glm::vec3 generate_asteroid_velocity(glm::vec3);
 
 
 struct Vector {
@@ -39,6 +62,28 @@ int main() {
     else
         std::cout << "Ray does not intersect the sphere." << std::endl;
 
+
+
+    //tester for the functions 
+    std::srand(static_cast<unsigned>(std::time(0)));
+    glm::vec3 position;
+    float radius;
+    glm::vec3 velocity;
+
+    for(int i = 0; i < 10; i++){
+        position = generate_asteroid_position();
+        radius = generate_asteroid_radius();
+        velocity = generate_asteroid_velocity(position);
+
+        printf("Position %f,%f,%f\n", position.x, position.y, position.z);
+        printf("Velocity %f,%f,%f\n", velocity.x, velocity.y, velocity.z);
+        printf("Radius %f\n", radius);
+        printf("\n");
+    }
+
+
+
+
     return 0;
 }
 
@@ -65,7 +110,7 @@ int TestRaySphere(Point p, Vector d, Sphere s) {
 
 bool TestSphereSphere(Sphere a, Sphere b) {
     // Calculate squared distance between centers
-  Vector d = a.position - b.position; //{a.position.x - b.position.x, a.position.y - b.position.y, a.position.z - b.position.z};
+    Vector d = a.position - b.position; //{a.position.x - b.position.x, a.position.y - b.position.y, a.position.z - b.position.z};
     float squaredDistance = Dot(d, d); // Same as d.LengthSquared()
 
     // Spheres intersect if squared distance is less than squared sum of radii
@@ -73,4 +118,48 @@ bool TestSphereSphere(Sphere a, Sphere b) {
     float squaredRadii = radiusSum * radiusSum;
 
     return squaredDistance <= squaredRadii;
+}
+
+
+
+
+glm::vec3 generate_asteroid_position(){
+
+    
+    float ro =  min_ro + (rand() % (max_ro - min_ro));
+    float theta = (rand() % (max_theta - min_theta)) - ((max_theta - min_theta) / 2.0 );
+    float phi = (rand() % (max_phi - min_phi)) - ((max_phi - min_phi) / 2.0 );
+
+    return {ro * cos(theta * M_PI / 180.0 ), ro * sin(theta * M_PI / 180.0), -ro * sin(phi * M_PI / 180.0)}; 
+}
+
+float generate_asteroid_radius(){
+
+    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(min_ast_size, max_ast_size);
+
+    // Generate a random float between 1 and 4
+    return dis(gen);
+}
+
+glm::vec3 generate_asteroid_velocity(glm::vec3 position){
+
+    
+    float theta = rand() % (max_theta - min_theta) - ((max_theta - min_theta) / 2.0);
+    float phi = rand() % (max_phi - min_phi) - ((max_phi - min_phi) / 2.0);
+    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(min_velocity_norm, max_velocity_norm);
+    float velocity_norm = dis(gen);
+
+    glm::vec3 velocity = {close_radius * cos(theta * M_PI / 180.0) - position. x, 
+                          close_radius * sin(theta * M_PI / 180.0) - position.y, 
+                          -close_radius * cos(phi * M_PI / 180.0) - position.z};
+
+
+
+    return glm::normalize(velocity) * velocity_norm; 
 }
